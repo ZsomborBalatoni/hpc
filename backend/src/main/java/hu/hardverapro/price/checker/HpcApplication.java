8c3619cff3ae46a798f0e8bbe6b0e52ce890f3e4
ZsomborBalatoni <zsombor.balatoni@gmail.com>
2024-03-11 23:01:35 +0100

    Inital commit
    backend

---
+++ backend/src/main/java/hu/hardverapro/price/checker/HpcApplication.java
@@ -0,0 +1,13 @@
+package hu.hardverapro.price.checker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HpcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HpcApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/hu/hardverapro/price/checker/HpcApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.hardverapro.price.checker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HpcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
