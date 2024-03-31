db8be259dc8b5514194039cfc80411baea0dd14a
ZsomborBalatoni <zsombor.balatoni@gmail.com>
2024-03-31 14:47:29 +0200

    Add base_url to hardware entity

--- backend/src/main/java/hu/hardverapro/price/checker/model/Hardware.java
+++ backend/src/main/java/hu/hardverapro/price/checker/model/Hardware.java
@@ -24,6 +24,9 @@ public class Hardware {
     @Column(nullable = false)
     private Long max_price;
 
+    @Column(nullable = false)
+    private String base_url;
+
     public Long getId() {
         return id;
     }
@@ -64,18 +67,28 @@ public class Hardware {
         this.max_price = max_price;
     }
 
+    public String getBase_url() {
+        return base_url;
+    }
+
+    public void setBase_url(String base_url) {
+        this.base_url = base_url;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
 
         if (!(o instanceof Hardware hardware)) return false;
 
-        return new EqualsBuilder().append(id, hardware.id).append(data_id, hardware.data_id).append(name, hardware.name).append(category, hardware.category).append(max_price, hardware.max_price).isEquals();
+        return new EqualsBuilder().append(id, hardware.id).append(data_id, hardware.data_id).append(name, hardware.name)
+                .append(category, hardware.category).append(max_price, hardware.max_price).append(base_url, hardware.base_url).isEquals();
     }
 
     @Override
     public int hashCode() {
-        return new HashCodeBuilder(17, 37).append(id).append(data_id).append(name).append(category).append(max_price).toHashCode();
+        return new HashCodeBuilder(17, 37).append(id).append(data_id).append(name)
+                .append(category).append(max_price).append(base_url).toHashCode();
     }
 
     @Override
@@ -85,7 +98,8 @@ public class Hardware {
                 ", data_id=" + data_id +
                 ", name='" + name + '\'' +
                 ", category='" + category + '\'' +
-                ", max_price=" + max_price +
+                ", max_price=" + max_price + '\'' +
+                ", baseUrl=" + base_url +
                 '}';
     }
 }
